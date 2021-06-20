10c1315083450e6e5ade29b134b320d909a111a0
zdenecek <zdnek.tomis@gmail.com>
2021-06-21 01:34:46 +0200

    update rainbow

--- rainbow.py
+++ rainbow.py
@@ -1,7 +1,8 @@
 north = 0
-east = 2
 south = 1
+east = 2
 west = 3
+
 
 def rotate_player(num, step, tables):
     tens = num // 10
@@ -11,15 +12,17 @@ def rotate_player(num, step, tables):
         new_units = tables
     return new_units + tens * 10
 
+
 def rotate_table(num, step, tables):
     num = (num + step + tables) % tables
     return tables if num == 0 else num
 
+
 def rainbow_paralel_boards(tables):
-    pad = lambda any: str(any).rjust(2)
-    
-    rotate = lambda num, step: rotate_player(num, step, tables)
-    rotate_t = lambda num, step: rotate_table(num, step, tables)
+    def pad(any): return str(any).rjust(2)
+
+    def rotate(num, step): return rotate_player(num, step, tables)
+    def rotate_t(num, step): return rotate_table(num, step, tables)
 
     table_rotations = {
         north: -1,
@@ -33,11 +36,13 @@ def rainbow_paralel_boards(tables):
     }
 
     for round in range(1, tables):
-        rounds[round] = [[rotate(player, 1) for  player in table] for table in rounds[round-1]]
+        rounds[round] = [[rotate(player, 1) for player in table]
+                         for table in rounds[round-1]]
 
     for table in range(tables):
         print('\n\n')
-        print(f'Individuál - {tables * 4} hráčů - {tables * 3} rozdání (Rainbow parallel)')
+        print(
+            f'Individuál - {tables * 4} hráčů - {tables * 3} rozdání (Rainbow parallel)')
         print('\n')
         print(f'Stůl číslo {table + 1}')
         print('\n')
@@ -45,24 +50,22 @@ def rainbow_paralel_boards(tables):
         print('=======================================================================')
 
         change = {
-            key: pad(rotate_t(table + 1, table_rotations[key])) for key in [north,south,east,west]
+            key: pad(rotate_t(table + 1, table_rotations[key])) for key in [north, south, east, west]
         }
 
         for round in rounds:
             data = rounds[round][table]
-            last = True if round == len(rounds) else False
+            last = True if round == len(rounds) - 1 else False
             print(
                 f'{pad(round * 3 + 1)}     |  {pad(data[north])} ( ..) |  {pad(data[south])} ( .E) |  {pad(data[east])} ( .S) |  {pad(data[west])} ( ..) |  {pad(round*3 + 1)}')
             print(
                 f'{pad(round * 3 + 2)}     |  {pad(data[north])} ( ..) |  {pad(data[east])} ( .W) |  {pad(data[south])} ( ..) |  {pad(data[west])} ( .S) |  {pad(round*3 + 2)} ')
             print(
                 f'{pad(round * 3 + 3)}     ' +
-                    f'|  {pad(data[north])} ({"  " if last else change[north]}N) ' +
-                    f'|  {pad(data[west])} ({"  " if last else change[west]}W) ' +
-                    f'|  {pad(data[south])} ({"  " if last else change[south]}S) ' +
-                    f'|  {pad(data[east])} ({"  " if last else change[east]}E) ' +
-                    f'|  {pad(round*3 + 3)}')
-            print(
-                f'{pad("")}     |  {pad("")}       |  {pad("")}       |  {pad("")}       |  {pad("")}       |  {pad("")}')
+                "".join([f'|  {pad(data[key])} ' + (f'({change[key]}N) ' if not last else "      ") for key in [north, west, south, east]]) +
+                f'|  {pad(round*3 + 3)}')
+            # print(f'{pad("")}     |' + f'  {pad("")}       |'*4 + f'  {pad("")}')
+            print('\n')
+
 
 rainbow_paralel_boards(7)
